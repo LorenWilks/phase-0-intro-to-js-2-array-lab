db59e697c860837c6e23944bd5949c5b5b220608
Loren Wilks <Loren.wilks@gmail.com>
2022-05-25 18:38:17 -0600

    commit tree 4cccbe22f64fe6ea6e4a724ed9242668dbe5fa03

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+   const cats = ["Milo", "Otis", "Garfield"];
+
+   function destructivelyAppendCat() {
+    cats.push("Ralph");
+   }
+   function destructivelyPrependCat(name) {
+       cats.unshift(name);
+   }
+   function destructivelyRemoveLastCat(){
+       cats.pop();
+   }
+   function destructivelyRemoveFirstCat() {
+       cats.shift();
+   }
+   function appendCat(){
+    const newCats = [...cats, "Broom"];
+    return newCats;
+   }
+   function prependCat(){
+    const newCats = ["Arnold", ...cats];
+    return newCats;
+   }
+   function removeLastCat(){
+    const newCats = cats.slice(0, cats.length - 1);
+    return newCats;
+   }
+   function removeFirstCat(){
+       const newCats = cats.slice(1);
+       return newCats;
+   }
